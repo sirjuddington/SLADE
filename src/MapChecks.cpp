289daf785649d423025215948f823c3066ea2d2f
sirjuddington <sirjuddington@gmail.com>
2013-12-11 18:17:56 +1030

    Added 'move thing' fix for stuck things map check, moves the thing in front of the line

--- src/MapChecks.cpp
+++ src/MapChecks.cpp
@@ -1095,6 +1095,31 @@ public:
 
 	bool fixProblem(unsigned index, unsigned fix_type, MapEditor* editor)
 	{
+		if (index >= things.size())
+			return false;
+
+		if (fix_type == 0)
+		{
+			MapThing* thing = things[index];
+			MapLine* line = lines[index];
+
+			// Get nearest line point to thing
+			fpoint2_t np = MathStuff::closestPointOnLine(thing->xPos(), thing->yPos(), line->x1(), line->y1(), line->x2(), line->y2());
+
+			// Get distance to move
+			double r = theGameConfiguration->thingType(thing->getType())->getRadius();
+			double dist = MathStuff::distance(0, 0, r, r);
+
+			editor->beginUndoRecord("Move Thing", true, false, false);
+
+			// Move along line direction
+			map->moveThing(thing->getIndex(), np.x - (line->frontVector().x * dist), np.y - (line->frontVector().y * dist));
+
+			editor->endUndoRecord();
+
+			return true;
+		}
+
 		return false;
 	}
 
