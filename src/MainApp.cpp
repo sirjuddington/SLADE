3493839364c31addfc26ed2305d358d39f0ae0e1
sirjuddington <sirjuddington@gmail.com>
2014-01-11 23:38:56 +1030

    Bump version to 3.1.0 beta 3

--- src/MainApp.cpp
+++ src/MainApp.cpp
@@ -72,7 +72,7 @@ namespace Global
 {
 	string error = "";
 
-	string version = "3.1.0 beta 2"
+	string version = "3.1.0 beta 3"
 #ifdef GIT_DESCRIPTION
 	                 " (" GIT_DESCRIPTION ")"
 #endif
