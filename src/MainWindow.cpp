a1f7b0b4827d66cffb8ea0a1f5ad704a5b4a15a9
sirjuddington <sirjuddington@gmail.com>
2014-03-11 21:48:48 +0930

    Fix start page not refreshing correctly in linux

--- src/MainWindow.cpp
+++ src/MainWindow.cpp
@@ -730,8 +730,11 @@ void MainWindow::onHTMLLinkClicked(wxEvent& e)
 	string href = ev.GetURL();
 
 #ifdef __WXGTK__
-	href.Replace("file://", "");
+	if (!href.EndsWith("startpage.htm"))
+		href.Replace("file://", "");
 #endif
+
+	//LOG_MESSAGE(2, "URL %s", href);
 
 	if (href.EndsWith("/"))
 		href.RemoveLast(1);
@@ -751,6 +754,7 @@ void MainWindow::onHTMLLinkClicked(wxEvent& e)
 
 		panel_archivemanager->handleAction(S_FMT("aman_recent%lu", index));
 		createStartPage();
+		html_startpage->Reload();
 	}
 	else if (href.StartsWith("action://"))
 	{
