ff0274bef9e0eea9787630506392c8898c7aeaed
sirjuddington <sirjuddington@gmail.com>
2014-03-21 19:34:28 +1030

    Always center SDialog on parent when shown

--- src/SDialog.h
+++ src/SDialog.h
@@ -11,8 +11,11 @@ public:
 	SDialog(wxWindow* parent, string title, string id, int width = -1, int height = -1, int x = -1, int y = -1);
 	virtual ~SDialog();
 
+	void	setSavedSize(int def_width = -1, int def_height = -1);
+
 	void	onSize(wxSizeEvent& e);
 	void	onMove(wxMoveEvent& e);
+	void	onShow(wxShowEvent& e);
 };
 
 #endif//__S_DIALOG_H__
