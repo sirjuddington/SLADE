8d9da6b0813ffb09fe72261b6ca4b02265305174
sirjuddington <sirjuddington@gmail.com>
2014-06-04 08:19:59 +0930

    Fix VersionCheck when no connection present

--- src/VersionCheck.cpp
+++ src/VersionCheck.cpp
@@ -1,21 +1,63 @@
 
+/*******************************************************************
+ * SLADE - It's a Doom Editor
+ * Copyright (C) 2008-2014 Simon Judd
+ *
+ * Email:       sirjuddington@gmail.com
+ * Web:         http://slade.mancubus.net
+ * Filename:    VersionCheck.cpp
+ * Description: VersionCheck class, a thread that checks online for
+ *              the latest version info and sends an event when done
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ *******************************************************************/
+
+
+/*******************************************************************
+ * INCLUDES
+ *******************************************************************/
 #include "Main.h"
 #include "WxStuff.h"
 #include "VersionCheck.h"
 #include <wx/protocol/http.h>
 #include <wx/sstream.h>
 
+
+/*******************************************************************
+ * VERSIONCHECK CLASS FUNCTIONS
+ *******************************************************************/
 wxDEFINE_EVENT(wxEVT_COMMAND_VERSIONCHECK_COMPLETED, wxThreadEvent);
 
+/* VersionCheck::VersionCheck
+ * VersionCheck class constructor
+ *******************************************************************/
 VersionCheck::VersionCheck(wxEvtHandler* handler) : wxThread()
 {
 	this->handler = handler;
 }
 
+/* VersionCheck::~VersionCheck
+ * VersionCheck class constructor
+ *******************************************************************/
 VersionCheck::~VersionCheck()
 {
 }
 
+/* VersionCheck::Entry
+ * VersionCheck thread entry function
+ *******************************************************************/
 wxThread::ExitCode VersionCheck::Entry()
 {
 	LOG_MESSAGE(3, "Starting VersionCheck thread");
@@ -31,14 +73,17 @@ wxThread::ExitCode VersionCheck::Entry()
 	while (!http.Connect("slade.mancubus.net"))
 	{
 		LOG_MESSAGE(3, "VersionCheck: No connection, waiting 1 sec");
-		wxSleep(1000);
+		wxSleep(1);
 		
+		// Max connection attempts
 		if (attempt_count++ > 5)
 		{
 			// Send (failed) event
 			wxThreadEvent* event = new wxThreadEvent(wxEVT_COMMAND_VERSIONCHECK_COMPLETED);
 			event->SetString("connect_failed");
 			wxQueueEvent(handler, event);
+
+			return NULL;
 		}
 	}
 
