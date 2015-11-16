4383212e67aecaad8c6be0c2647166b89852d76b
Eevee (Lexy Munroe) <eevee.git@veekun.com>
2015-11-16 10:38:58 -0800

    Always update the special when clicking in the treeview
    Fallout from #325.

--- src/ActionSpecialDialog.cpp
+++ src/ActionSpecialDialog.cpp
@@ -856,6 +856,7 @@ ActionSpecialPanel::ActionSpecialPanel(wxWindow* parent, bool trigger) : wxPanel
 	SetSizerAndFit(sizer);
 
 	// Bind events
+	tree_specials->Bind(wxEVT_DATAVIEW_SELECTION_CHANGED, &ActionSpecialPanel::onSpecialSelectionChanged, this);
 	tree_specials->Bind(wxEVT_DATAVIEW_ITEM_ACTIVATED, &ActionSpecialPanel::onSpecialItemActivated, this);
 }
 
@@ -1175,10 +1176,10 @@ void ActionSpecialPanel::onRadioButtonChanged(wxCommandEvent& e)
 	showGeneralised(rb_generalised->GetValue());
 }
 
-/* ActionSpecialPanel::SpecialItemActivated
- * Called when the action special item is activated
+/* ActionSpecialPanel::onSpecialSelectionChanged
+ * Called when the action special selection is changed
  *******************************************************************/
-void ActionSpecialPanel::onSpecialItemActivated(wxDataViewEvent &e)
+void ActionSpecialPanel::onSpecialSelectionChanged(wxDataViewEvent &e)
 {
 	if ((theGameConfiguration->isBoom() && rb_generalised->GetValue()))
 	{
@@ -1199,9 +1200,18 @@ void ActionSpecialPanel::onSpecialItemActivated(wxDataViewEvent &e)
 			arg_values[a] = panel_args->getArgValue(a);
 		panel_args->setup(&args);
 		panel_args->setValues(arg_values);
-		// Go to args tab
-		panel_args->SetFocus();
 	}
+}
+
+/* ActionSpecialPanel::SpecialItemActivated
+ * Called when the action special item is activated (double-clicked or
+ * enter pressed)
+ *******************************************************************/
+void ActionSpecialPanel::onSpecialItemActivated(wxDataViewEvent &e)
+{
+	// Jump to args tab, if there is one
+	if (panel_args)
+		panel_args->SetFocus();
 }
 
 /* ActionSpecialPanel::onSpecialTextChanged
