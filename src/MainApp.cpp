adc0ddc671886531d7b43ad64e09773f2c06d24c
sirjuddington <sirjuddington@gmail.com>
2015-12-12 18:19:08 +1030

    Bump version to 3.1.1 beta 2

--- src/MainApp.cpp
+++ src/MainApp.cpp
@@ -80,9 +80,9 @@ namespace Global
 {
 	string error = "";
 
-	int beta_num = 1;
+	int beta_num = 2;
 	int version_num = 3110;
-	string version = "3.1.1 Beta 1";
+	string version = "3.1.1 Beta 2";
 #ifdef GIT_DESCRIPTION
 	string sc_rev = GIT_DESCRIPTION;
 #else
