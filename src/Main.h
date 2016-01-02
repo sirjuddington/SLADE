ff6906af2ad15b5e36b656dd17e5b59a9c12ad7f
Eevee (Lexy Munroe) <eevee.git@veekun.com>
2016-01-02 14:00:42 -0800

    Insert a space between LOG_DEBUG arguments
    I'm used to this from Python, and I don't see any reason you wouldn't
    want it here.

--- src/Main.h
+++ src/Main.h
@@ -187,18 +187,19 @@ inline void LOG_DEBUG(
 )
 {
 	string message;
-	message << a1.get();
-	message << a2.get();
-	message << a3.get();
-	message << a4.get();
-	message << a5.get();
-	message << a6.get();
-	message << a7.get();
-	message << a8.get();
-	message << a9.get();
-	message << a10.get();
-	message << a11.get();
+	message << a1.get() << " ";
+	message << a2.get() << " ";
+	message << a3.get() << " ";
+	message << a4.get() << " ";
+	message << a5.get() << " ";
+	message << a6.get() << " ";
+	message << a7.get() << " ";
+	message << a8.get() << " ";
+	message << a9.get() << " ";
+	message << a10.get() << " ";
+	message << a11.get() << " ";
 	message << a12.get();
+	message.Trim();
 	wxLogMessage("%s", message);
 }
 
