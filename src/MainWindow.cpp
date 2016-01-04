3c5e17bcf8e3c81499512babc8d40358b3fbf648
sirjuddington <sirjuddington@gmail.com>
2016-01-04 18:30:54 +1030

    Set focus to toolbar initially so that menu shortcuts work at startup
    Apparently wxWebView is hard coded to ignore some common shortcuts like Ctrl+O or Ctrl+N, to stop IE from doing things it shouldn't.
    
    Thing is, it just eats the event instead of passing it on. That won't cause any problems now will it? >_>

--- src/MainWindow.cpp
+++ src/MainWindow.cpp
@@ -410,6 +410,9 @@ void MainWindow::setupLayout()
 	Bind(wxEVT_AUINOTEBOOK_PAGE_CHANGED, &MainWindow::onTabChanged, this);
 	Bind(wxEVT_STOOLBAR_LAYOUT_UPDATED, &MainWindow::onToolBarLayoutChanged, this, toolbar->GetId());
 	Bind(wxEVT_ACTIVATE, &MainWindow::onActivate, this);
+
+	// Initial focus to toolbar
+	toolbar->SetFocus();
 }
 
 #ifdef USE_WEBVIEW_STARTPAGE
@@ -430,10 +433,6 @@ void MainWindow::createStartPage(bool newtip)
 	ArchiveEntry* entry_tips = res_archive->entryAtPath("tips.txt");
 	export_entries.push_back(res_archive->entryAtPath("logo.png"));
 	export_entries.push_back(res_archive->entryAtPath("html/box-title-back.png"));
-	//export_entries.push_back(res_archive->entryAtPath("icons/e_archive.png"));
-	//export_entries.push_back(res_archive->entryAtPath("icons/e_wad.png"));
-	//export_entries.push_back(res_archive->entryAtPath("icons/e_zip.png"));
-	//export_entries.push_back(res_archive->entryAtPath("icons/e_folder.png"));
 
 	// Can't do anything without html entry
 	if (!entry_html)
@@ -531,10 +530,6 @@ void MainWindow::createStartPage(bool newtip)
 #ifdef __WXMSW__
 	html_startpage->Reload();
 #endif
-
-	// Clean up
-	//wxRemoveFile(html_file);
-	//wxRemoveFile(appPath("logo.png", DIR_TEMP));
 }
 
 #else
