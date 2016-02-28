f0651c036212d8b32625d26613d7710141ada24e
Simon Judd <sirjuddington@gmail.com>
2016-02-28 23:12:28 +1030

    Bump version to 3.1.2 alpha

--- src/Application/MainApp.cpp
+++ src/Application/MainApp.cpp
@@ -85,8 +85,8 @@ namespace Global
 	string error = "";
 
 	int beta_num = 0;
-	int version_num = 3110;
-	string version = "3.1.1";
+	int version_num = 3120;
+	string version = "3.1.2 Alpha";
 #ifdef GIT_DESCRIPTION
 	string sc_rev = GIT_DESCRIPTION;
 #else
