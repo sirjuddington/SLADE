ef571cfd350ef111cb7c16ffeca22acda15212e7
Simon Judd <sirjuddington@gmail.com>
2016-03-09 19:46:29 +1030

    Bump version to 3.1.1.1

--- src/Application/MainApp.cpp
+++ src/Application/MainApp.cpp
@@ -85,8 +85,8 @@ namespace Global
 	string error = "";
 
 	int beta_num = 0;
-	int version_num = 3110;
-	string version = "3.1.1";
+	int version_num = 3111;
+	string version = "3.1.1.1";
 #ifdef GIT_DESCRIPTION
 	string sc_rev = GIT_DESCRIPTION;
 #else
