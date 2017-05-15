b7c8edb7a38360666091dff045487f32569e8e5f
Unknown <sirjuddington@gmail.com>
2017-05-15 12:11:17 +0930

    Clean up some parser/tree stuff
    Added a few helper functions to the Parser class to avoid the need to cast from STreeNode to ParseTreeNode so much. Eventually I'd like to implement a templated tree class or something to avoid this, but that gets kinda complicated and the current one works well enough, albeit messily.
    
    I've also adopted a new header/boilerplate comment style which I'll use from now on. I know it's not really necessary but I was getting sick of having visual studio screw with the formatting when copy+pasting them. As with the new class member naming I'll update files with the new style as I go along rather than do everything at once, which would take forever :P

--- src/Game/Args.cpp
+++ src/Game/Args.cpp
@@ -1,11 +1,55 @@
+
+// ----------------------------------------------------------------------------
+// SLADE - It's a Doom Editor
+// Copyright(C) 2008 - 2017 Simon Judd
+//
+// Email:       sirjuddington@gmail.com
+// Web:         https://slade.mancubus.net
+// Filename:    Args.cpp
+// Description: Arg and related structs for ActionSpecial and ThingType arg
+//              definitions
+//
+// This program is free software; you can redistribute it and/or modify it
+// under the terms of the GNU General Public License as published by the Free
+// Software Foundation; either version 2 of the License, or (at your option)
+// any later version.
+//
+// This program is distributed in the hope that it will be useful, but WITHOUT
+// ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or 
+// FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for
+// more details.
+//
+// You should have received a copy of the GNU General Public License along with
+// this program; if not, write to the Free Software Foundation, Inc.,
+// 51 Franklin Street, Fifth Floor, Boston, MA  02110 - 1301, USA.
+// ----------------------------------------------------------------------------
+
+
+// ----------------------------------------------------------------------------
+//
+// Includes
+//
+// ----------------------------------------------------------------------------
 #include "Main.h"
 #include "Args.h"
 #include "Utility/Parser.h"
 
 using namespace Game;
 
+
+// ----------------------------------------------------------------------------
+//
+// Local Functions
+//
+// ----------------------------------------------------------------------------
 namespace
 {
+	// ------------------------------------------------------------------------
+	// customFlags
+	//
+	// Returns a string representation of [value] for a 'custom flags' type
+	// arg, given [custom_flags]
+	// ------------------------------------------------------------------------
 	string customFlags(int value, const vector<ArgValue>& custom_flags)
 	{
 		// This has to go in REVERSE order to correctly handle multi-bit
@@ -17,7 +61,7 @@ namespace
 		bool has_flag;
 		for (auto it = custom_flags.rbegin();
 			it != custom_flags.rend();
-			it++)
+			++it)
 		{
 			if ((it->value & (it->value - 1)) != 0)
 				// Not a power of two, so must be a group
@@ -48,7 +92,7 @@ namespace
 		while (true)
 		{
 			out += *it;
-			it++;
+			++it;
 			if (it == flags.rend())
 				break;
 			out += " + ";
@@ -57,6 +101,19 @@ namespace
 	}
 }
 
+
+// ----------------------------------------------------------------------------
+//
+// Arg Struct Functions
+//
+// ----------------------------------------------------------------------------
+
+
+// ----------------------------------------------------------------------------
+// Arg::valueString
+//
+// Returns a string representation of [value] depending on the arg's type
+// ----------------------------------------------------------------------------
 string Arg::valueString(int value)
 {
 	switch (type)
@@ -101,11 +158,16 @@ string Arg::valueString(int value)
 	return S_FMT("%d", value);
 }
 
+// ----------------------------------------------------------------------------
+// Arg::speedLabel
+//
+// Returns a string representation of speed [value]
+// ----------------------------------------------------------------------------
 string Arg::speedLabel(int value)
 {
 	// Speed can optionally have a set of predefined values, most taken
 	// from the Boom generalized values
-	if (!custom_values.size())
+	if (custom_values.empty())
 		return "";
 	if (value == 0)
 		return "broken";
@@ -123,10 +185,14 @@ string Arg::speedLabel(int value)
 	return "";
 }
 
+// ----------------------------------------------------------------------------
+// Arg::parse
+//
+// Parses an arg definition from [node], using [shared_args] for predeclared
+// args if it is given
+// ----------------------------------------------------------------------------
 void Arg::parse(ParseTreeNode* node, SpecialMap* shared_args)
 {
-	ParseTreeNode* custom = nullptr;
-
 	// Check for simple definition
 	if (node->isLeaf())
 	{
@@ -157,15 +223,15 @@ void Arg::parse(ParseTreeNode* node, SpecialMap* shared_args)
 		// Extended arg definition
 
 		// Name
-		auto val = (ParseTreeNode*)node->getChild("name");
+		auto val = node->getChildPTN("name");
 		if (val) name = val->getStringValue();
 
 		// Description
-		val = (ParseTreeNode*)node->getChild("desc");
+		val = node->getChildPTN("desc");
 		if (val) desc = val->getStringValue();
 
 		// Type
-		val = (ParseTreeNode*)node->getChild("type");
+		val = node->getChildPTN("type");
 		string atype;
 		if (val) atype = val->getStringValue();
 		if (S_CMPNOCASE(atype, "yesno"))
@@ -184,34 +250,35 @@ void Arg::parse(ParseTreeNode* node, SpecialMap* shared_args)
 			type = Number;
 
 		// Customs
-		val = (ParseTreeNode*)node->getChild("custom_values");
+		val = node->getChildPTN("custom_values");
 		if (val)
 		{
-			for (unsigned b = 0; b < val->nChildren(); b++)
-			{
-				custom = (ParseTreeNode*)val->getChild(b);
-				custom_values.push_back({
-					custom->getStringValue(),
-					atoi(CHR(custom->getName()))
-				});
-			}
+			for (auto cv : val->allChildren())
+				custom_values.push_back({ Parser::node(cv)->getStringValue(), atoi(CHR(cv->getName())) });
 		}
 
-		val = (ParseTreeNode*)node->getChild("custom_flags");
+		val = node->getChildPTN("custom_flags");
 		if (val)
 		{
-			for (unsigned b = 0; b < val->nChildren(); b++)
-			{
-				custom = (ParseTreeNode*)val->getChild(b);
-				custom_flags.push_back({
-					custom->getStringValue(),
-					atoi(CHR(custom->getName()))
-				});
-			}
+			for (auto cf : val->allChildren())
+				custom_flags.push_back({ Parser::node(cf)->getStringValue(), atoi(CHR(cf->getName())) });
 		}
 	}
 }
 
+
+// ----------------------------------------------------------------------------
+//
+// ArgSpec Struct Functions
+//
+// ----------------------------------------------------------------------------
+
+
+// ----------------------------------------------------------------------------
+// ArgSpec::stringDesc
+//
+// Returns a string representation of [values] depending on the spec arg types
+// ----------------------------------------------------------------------------
 string ArgSpec::stringDesc(int values[5], string values_str[2])
 {
 	string ret;
