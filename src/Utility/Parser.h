61f48be9807db8169f4e5efbef20c35d5b37dfca
Unknown <sirjuddington@gmail.com>
2017-05-23 14:03:15 +0930

    Fix merge glitch

--- src/Utility/Parser.h
+++ src/Utility/Parser.h
@@ -37,10 +37,6 @@ public:
 	ParseTreeNode*	getChildPTN(const string& name) { return static_cast<ParseTreeNode*>(getChild(name)); }
 	ParseTreeNode*	getChildPTN(unsigned index) { return static_cast<ParseTreeNode*>(getChild(index)); }
 
-	// To avoid need for casts everywhere
-	ParseTreeNode*	getChildPTN(const string& name) { return static_cast<ParseTreeNode*>(getChild(name)); }
-	ParseTreeNode*	getChildPTN(unsigned index) { return static_cast<ParseTreeNode*>(getChild(index)); }
-
 	bool	parse(Tokenizer& tz);
 
 	typedef std::unique_ptr<ParseTreeNode> UPtr;
