09c4a14947bae7addd1b6ece15e754a87752288d
Unknown <sirjuddington@gmail.com>
2017-07-19 15:38:13 +0930

    Use SFML HTTP implementation instead of wx for WebGet

--- src/General/WebGet.cpp
+++ src/General/WebGet.cpp
@@ -1,6 +1,7 @@
 
 #include "Main.h"
 #include "WebGet.h"
+#include <SFML/Network.hpp>
 
 wxDEFINE_EVENT(wxEVT_THREAD_WEBGET_COMPLETED, wxThreadEvent);
 
@@ -11,61 +12,84 @@ WebGet::WebGet(wxEvtHandler* handler, string host, string path) :
 	path_{ path }
 {
 	// Ensure path begins with /
-	if (!path.StartsWith("/"))
-		path.Prepend("/");
+	if (!path_.StartsWith("/"))
+		path_.Prepend("/");
 }
 
 wxThread::ExitCode WebGet::Entry()
 {
-	// Init HTTP
-	wxHTTP http;
-	http.SetHeader("Content-type", "text/html; charset=utf-8");
-	http.SetTimeout(10);
+	sf::Http http(CHR(host_));
 
-	// Wait for connection
-	Log::info(3, S_FMT("WebGet: Testing connection to %s...", CHR(host_)));
-	int attempt_count = 0;
-	while (!http.Connect(host_))
+	sf::Http::Request request;
+	request.setMethod(sf::Http::Request::Get);
+	request.setUri(CHR(path_));
+
+	auto response = http.sendRequest(request);
+
+	if (response.getStatus() == sf::Http::Response::Ok)
 	{
-		Log::info(3, "WebGet: No connection, waiting 1 sec");
-		wxSleep(1);
-
-		// Max connection attempts
-		if (attempt_count++ > 5)
-		{
-			// Send (failed) event
-			auto event = new wxThreadEvent(wxEVT_THREAD_WEBGET_COMPLETED);
-			event->SetString("connect_failed");
-			wxQueueEvent(handler_, event);
-
-			return nullptr;
-		}
-	}
-
-	// Get data
-	Log::info(3, "WebGet: Retrieving data...");
-	auto stream = http.GetInputStream(path_);
-	string data;
-	if (http.GetError() == wxPROTO_NOERR)
-	{
-		wxStringOutputStream out(&data);
-		stream->Read(out);
-
-		Log::info(3, S_FMT("WebGet: Got data successfully:\n%s", CHR(data)));
+		// Send event
+		auto event = new wxThreadEvent(wxEVT_THREAD_WEBGET_COMPLETED);
+		event->SetString(response.getBody());
+		wxQueueEvent(handler_, event);
 	}
 	else
 	{
-		Log::info(3, S_FMT("WebGet: Error connecting to %s", CHR(host_)));
+		// Send (failed) event
+		auto event = new wxThreadEvent(wxEVT_THREAD_WEBGET_COMPLETED);
+		event->SetString("connect_failed");
+		wxQueueEvent(handler_, event);
 	}
 
-	// Clean up
-	delete stream;
-	http.Close();
+	//// Init HTTP
+	//wxHTTP http;
+	//http.SetHeader("Content-type", "text/html; charset=utf-8");
+	//http.SetTimeout(10);
 
-	// Send event
-	auto event = new wxThreadEvent(wxEVT_THREAD_WEBGET_COMPLETED);
-	event->SetString(data);
-	wxQueueEvent(handler_, event);
+	//// Wait for connection
+	//Log::info(3, S_FMT("WebGet: Testing connection to %s...", CHR(host_)));
+	//int attempt_count = 0;
+	//while (!http.Connect(host_))
+	//{
+	//	Log::info(3, "WebGet: No connection, waiting 1 sec");
+	//	wxSleep(1);
+
+	//	// Max connection attempts
+	//	if (attempt_count++ > 5)
+	//	{
+	//		// Send (failed) event
+	//		auto event = new wxThreadEvent(wxEVT_THREAD_WEBGET_COMPLETED);
+	//		event->SetString("connect_failed");
+	//		wxQueueEvent(handler_, event);
+
+	//		return nullptr;
+	//	}
+	//}
+
+	//// Get data
+	//Log::info(3, "WebGet: Retrieving data...");
+	//auto stream = http.GetInputStream(path_);
+	//string data;
+	//if (http.GetError() == wxPROTO_NOERR)
+	//{
+	//	wxStringOutputStream out(&data);
+	//	stream->Read(out);
+
+	//	Log::info(3, S_FMT("WebGet: Got data successfully:\n%s", CHR(data)));
+	//}
+	//else
+	//{
+	//	Log::info(3, S_FMT("WebGet: Error connecting to %s", CHR(host_)));
+	//}
+
+	//// Clean up
+	//delete stream;
+	//http.Close();
+
+	//// Send event
+	//auto event = new wxThreadEvent(wxEVT_THREAD_WEBGET_COMPLETED);
+	//event->SetString(data);
+	//wxQueueEvent(handler_, event);
 
 	return nullptr;
 }
