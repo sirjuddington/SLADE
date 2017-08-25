8a48d1b93082accf9d0c344b8f579c7619424adc
Simon Judd <sirjuddington@gmail.com>
2017-08-25 11:01:12 +0930

    Add 'Extra Line Height' setting to text editor preferences
    Also rearranged the preferences page a bit

--- src/Dialogs/Preferences/TextEditorPrefsPanel.h
+++ src/Dialogs/Preferences/TextEditorPrefsPanel.h
@@ -1,37 +1,34 @@
-
-#ifndef __TEXT_EDITOR_PREFS_PANEL_H__
-#define __TEXT_EDITOR_PREFS_PANEL_H__
+#pragma once
 
 #include "PrefsPanelBase.h"
 
 class TextEditorPrefsPanel : public PrefsPanelBase
 {
-private:
-	wxSpinCtrl*	spin_tab_width;
-	wxCheckBox*	cb_auto_indent;
-	wxCheckBox*	cb_trim_whitespace;
-	wxCheckBox*	cb_syntax_hilight;
-	wxCheckBox*	cb_brace_match;
-	wxSpinCtrl*	spin_right_margin;
-	wxCheckBox*	cb_indent_guides;
-	wxCheckBox*	cb_calltips_mouse;
-	wxCheckBox*	cb_calltips_parenthesis;
-	wxCheckBox*	cb_calltips_colourise;
-	wxCheckBox*	cb_calltips_dim_optional;
-	wxCheckBox*	cb_calltips_use_font;
-	wxCheckBox*	cb_fold_enable;
-	wxCheckBox*	cb_fold_comments;
-	wxCheckBox*	cb_fold_preprocessor;
-	wxCheckBox*	cb_fold_lines;
-	wxCheckBox*	cb_match_cursor_word;
-	wxChoice*	choice_line_hilight;
-
 public:
 	TextEditorPrefsPanel(wxWindow* parent);
 	~TextEditorPrefsPanel();
 
-	void	init();
-	void	applyPreferences();
-};
+	void	init() override;
+	void	applyPreferences() override;
 
-#endif//__TEXT_EDITOR_PREFS_PANEL_H__
+private:
+	wxSpinCtrl*	spin_tab_width_;
+	wxCheckBox*	cb_auto_indent_;
+	wxCheckBox*	cb_trim_whitespace_;
+	wxCheckBox*	cb_syntax_hilight_;
+	wxCheckBox*	cb_brace_match_;
+	wxSpinCtrl*	spin_right_margin_;
+	wxSpinCtrl*	spin_line_spacing_;
+	wxCheckBox*	cb_indent_guides_;
+	wxCheckBox*	cb_calltips_mouse_;
+	wxCheckBox*	cb_calltips_parenthesis_;
+	wxCheckBox*	cb_calltips_colourise_;
+	wxCheckBox*	cb_calltips_dim_optional_;
+	wxCheckBox*	cb_calltips_use_font_;
+	wxCheckBox*	cb_fold_enable_;
+	wxCheckBox*	cb_fold_comments_;
+	wxCheckBox*	cb_fold_preprocessor_;
+	wxCheckBox*	cb_fold_lines_;
+	wxCheckBox*	cb_match_cursor_word_;
+	wxChoice*	choice_line_hilight_;
+};
