09ebc79eeb8a060101bc43d9f888b9e35a2ad2b1
Unknown <sirjuddington@gmail.com>
2018-04-10 22:34:06 +0930

    Add timestamp to binary 7zs if using git revision in version

--- dist/makebuild.ps1
+++ dist/makebuild.ps1
@@ -1,5 +1,5 @@
 $version = "3120_b5"
-$versionstring = "3.1.2 Beta 5"
+$rev_short = Invoke-Expression "git.exe rev-parse --short HEAD"
 
 # Check for 7-zip install
 $7zpath = "$env:ProgramFiles\7-Zip\7z.exe"
@@ -8,18 +8,14 @@ if (-not (Test-Path $7zpath))
 	Write-Host "7-zip is required to make slade.pk3 and the release binaries archive" -foregroundcolor red
 }
 
-# Prompt for version number
-#$version = Read-Host -Prompt "Enter a version number (eg. 3110)"
-
-# Prompt for beta number
-#$beta = Read-Host -Prompt "Enter a beta number (blank for none)"
-
-# Determine full version string
-#$versionstring = $version
-#if ($beta)
-#{
-#    $versionstring = $version+"_b"+$beta
-#}
+# Prompt to use git revision
+Write-Host "`nUse git revision in version? ($rev_short) (y/n) " -foregroundcolor cyan -nonewline
+$userev = Read-Host
+if ($userev.ToLower() -eq "y")
+{
+	$env:CL = "/DGIT_DESCRIPTION=`"\`"$rev_short\`"`""
+	$version = "${version}_$rev_short"
+}
 
 # Prompt to build SLADE
 Write-Host "`nRebuild SLADE? (y/n) " -foregroundcolor cyan -nonewline
@@ -28,18 +24,6 @@ $buildbinaries = Read-Host
 # Build SLADE
 if ($buildbinaries.ToLower() -eq "y")
 {
-	Write-Host "`nUse git revision? (y/n) " -foregroundcolor cyan -nonewline
-	$userev = Read-Host
-
-	$rev_full = iex "git.exe rev-parse HEAD"
-	$rev_short = iex "git.exe rev-parse --short HEAD"
-	Write-Host "`nGit Rev: $rev_short"
-
-	if ($userev.ToLower() -eq "y")
-	{
-		$env:CL = "/DGIT_DESCRIPTION=`"\`"$rev_short\`"`""
-		$version = "${version}_$rev_short"
-	}
 	$devenvpath = "${env:ProgramFiles(x86)}\Microsoft Visual Studio 14.0\Common7\IDE\devenv.com"
 	& $devenvpath (resolve-path ..\build\msvc\SLADE.sln).Path /rebuild Release /project SLADE.vcxproj
 	& $devenvpath (resolve-path ..\build\msvc\SLADE.sln).Path /rebuild "Release - WinXP" /project SLADE.vcxproj
@@ -75,7 +59,6 @@ Write-Host "Done" -foregroundcolor green
 Write-Host "`nCopying SLADE files..." -foregroundcolor yellow
 Copy-Item (resolve-path ".\FreeImage.dll")      "$releasedir" -Force
 Copy-Item (resolve-path ".\libfluidsynth.dll")  "$releasedir" -Force
-Copy-Item (resolve-path ".\openal32.dll")		"$releasedir" -Force
 Copy-Item (resolve-path ".\SLADE.exe")          "$releasedir" -Force
 Copy-Item (resolve-path ".\SLADE.pdb")          "$releasedir" -Force
 Copy-Item (resolve-path ".\WinXP\SLADE.exe")    "$releasedir\XP" -Force
@@ -90,8 +73,17 @@ $buildbinaries = Read-Host
 # Build binaries 7z
 if ($buildbinaries.ToLower() -eq "y")
 {
+	# Append timestamp to filenames if git rev is used
+	$timestamp = Get-Date -Format FileDate
+	if ($userev.ToLower() -eq "y") {
+		$timestamp = "_${timestamp}"
+	}
+	else {
+		$timestamp = ""
+	}
+
 	Write-Host "`nBuiling binaries 7z..." -foregroundcolor yellow
-	& $7zpath a -t7z "$releasedir\slade_$version.7z" `
+	& $7zpath a -t7z "$releasedir\slade_${version}${timestamp}.7z" `
 	"$releasedir\FreeImage.dll" `
 	"$releasedir\libfluidsynth.dll" `
 	"$releasedir\openal32.dll" `
@@ -101,7 +93,7 @@ if ($buildbinaries.ToLower() -eq "y")
 	Write-Host "Done" -foregroundcolor green
 
 	Write-Host "`nBuilding XP binary 7z..." -ForegroundColor Yellow
-	& $7zpath a -t7z "$releasedir\slade_${version}_winxp.7z" `
+	& $7zpath a -t7z "$releasedir\slade_${version}_winxp${timestamp}.7z" `
 	"$releasedir\FreeImage.dll" `
 	"$releasedir\libfluidsynth.dll" `
 	"$releasedir\openal32.dll" `
