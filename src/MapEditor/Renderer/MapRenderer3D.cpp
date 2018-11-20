aa5f0460d93b9d59df7c6c7d77096aae2947ff5c
Kevin Caccamo <kevin@ciinet.org>
2018-11-20 00:47:48 -0500

    Fix UV coordinates and scale of scaled textures
    Fix UV coordinates and scale for quads that use scaled textures.
    Set sx and sy to the scale specified in the texture definition.
    Add world_panning field to GLTexture so that the "World panning" flag is passed to the renderer, and set it in MapTextureManager.getTexture/getFlat.
    Use GLTexture worldPanning to determine how UVs of quads should be manipulated.

--- src/MapEditor/Renderer/MapRenderer3D.cpp
+++ src/MapEditor/Renderer/MapRenderer3D.cpp
@@ -1185,8 +1185,6 @@ void MapRenderer3D::setupQuadTexCoords(MapRenderer3D::quad_3d_t* quad, int lengt
 		y1 = y2 - height;
 	}
 
-	sx *= quad->texture->getScaleX();
-	sy *= quad->texture->getScaleY();
 	double x_mult = 1.0 / (quad->texture->getWidth() * sx);
 	double y_mult = 1.0 / (quad->texture->getHeight() * sy);
 
@@ -1285,7 +1283,9 @@ void MapRenderer3D::updateLine(unsigned index)
 		}
 
 		// Texture scale
-		sx = sy = 1;
+		quad.texture = MapEditor::textureManager().getTexture(line->s1()->getTexMiddle(), mixed);
+		sx = quad.texture->getScaleX();
+		sy = quad.texture->getScaleY();
 		if (Game::configuration().featureSupported(UDMFFeature::TextureScaling))
 		{
 			if (line->s1()->hasProp("scalex_mid"))
@@ -1293,15 +1293,16 @@ void MapRenderer3D::updateLine(unsigned index)
 			if (line->s1()->hasProp("scaley_mid"))
 				sy = 1.0 / line->s1()->floatProperty("scaley_mid");
 		}
-		xoff *= sx;
-		yoff *= sy;
+		if (!quad.texture->worldPanning()) {
+			xoff *= sx;
+			yoff *= sy;
+		}
 
 		// Create quad
 		setupQuad(&quad, line->x1(), line->y1(), line->x2(), line->y2(), cp1, fp1);
 		quad.colour = colour1;
 		quad.fogcolour = fogcolour1;
 		quad.light = light1;
-		quad.texture = MapEditor::textureManager().getTexture(line->s1()->getTexMiddle(), mixed);
 		setupQuadTexCoords(&quad, length, xoff, yoff, ceiling1, floor1, lpeg, sx, sy);
 
 		// Add middle quad and finish
@@ -1376,7 +1377,9 @@ void MapRenderer3D::updateLine(unsigned index)
 		}
 
 		// Texture scale
-		sx = sy = 1;
+		quad.texture = MapEditor::textureManager().getTexture(line->s1()->getTexLower(), mixed);
+		sx = quad.texture->getScaleX();
+		sy = quad.texture->getScaleY();
 		if (map->currentFormat() == MAP_UDMF && Game::configuration().featureSupported(UDMFFeature::TextureScaling))
 		{
 			if (line->s1()->hasProp("scalex_bottom"))
@@ -1384,8 +1387,10 @@ void MapRenderer3D::updateLine(unsigned index)
 			if (line->s1()->hasProp("scaley_bottom"))
 				sy = 1.0 / line->s1()->floatProperty("scaley_bottom");
 		}
-		xoff *= sx;
-		yoff *= sy;
+		if (!quad.texture->worldPanning()) {
+			xoff *= sx;
+			yoff *= sy;
+		}
 
 		if (lpeg)	// Lower unpegged
 			yoff += (ceiling1 - floor2);
@@ -1395,7 +1400,6 @@ void MapRenderer3D::updateLine(unsigned index)
 		quad.colour = colour1;
 		quad.fogcolour = fogcolour1;
 		quad.light = light1;
-		quad.texture = MapEditor::textureManager().getTexture(line->s1()->getTexLower(), mixed);
 		setupQuadTexCoords(&quad, length, xoff, yoff, floor2, floor1, false, sx, sy);
 		// No, the sky hack is only for ceilings!
 		// if (S_CMPNOCASE(sky_flat, line->backSector()->getFloorTex())) quad.flags |= SKY;
@@ -1427,16 +1431,19 @@ void MapRenderer3D::updateLine(unsigned index)
 		}
 
 		// Texture scale
-		sx = sy = 1;
+		sx = quad.texture->getScaleX();
+		sy = quad.texture->getScaleY();
 		if (map->currentFormat() == MAP_UDMF && Game::configuration().featureSupported(UDMFFeature::TextureScaling))
 		{
 			if (line->s1()->hasProp("scalex_mid"))
-				sx = 1.0 / line->s1()->floatProperty("scalex_mid");
+				sx *= 1.0 / line->s1()->floatProperty("scalex_mid");
 			if (line->s1()->hasProp("scaley_mid"))
-				sy = 1.0 / line->s1()->floatProperty("scaley_mid");
+				sy *= 1.0 / line->s1()->floatProperty("scaley_mid");
 		}
-		xoff *= sx;
-		yoff *= sy;
+		if (!quad.texture->worldPanning()) {
+			xoff *= sx;
+			yoff *= sy;
+		}
 
 		// Setup quad coordinates
 		double top, bottom;
@@ -1503,7 +1510,9 @@ void MapRenderer3D::updateLine(unsigned index)
 		}
 
 		// Texture scale
-		sx = sy = 1;
+		quad.texture = MapEditor::textureManager().getTexture(line->s1()->getTexUpper(), mixed);
+		sx = quad.texture->getScaleX();
+		sy = quad.texture->getScaleY();
 		if (map->currentFormat() == MAP_UDMF && Game::configuration().featureSupported(UDMFFeature::TextureScaling))
 		{
 			if (line->s1()->hasProp("scalex_top"))
@@ -1511,15 +1520,16 @@ void MapRenderer3D::updateLine(unsigned index)
 			if (line->s1()->hasProp("scaley_top"))
 				sy = 1.0 / line->s1()->floatProperty("scaley_top");
 		}
-		xoff *= sx;
-		yoff *= sy;
+		if (!quad.texture->worldPanning()) {
+			xoff *= sx;
+			yoff *= sy;
+		}
 
 		// Create quad
 		setupQuad(&quad, line->x1(), line->y1(), line->x2(), line->y2(), cp1, cp2);
 		quad.colour = colour1;
 		quad.fogcolour = fogcolour1;
 		quad.light = light1;
-		quad.texture = MapEditor::textureManager().getTexture(line->s1()->getTexUpper(), mixed);
 		setupQuadTexCoords(&quad, length, xoff, yoff, ceiling1, ceiling2, !upeg, sx, sy);
 		// Sky hack only applies if both sectors have a sky ceiling
 		if (S_CMPNOCASE(sky_flat, line->frontSector()->getCeilingTex()) && S_CMPNOCASE(sky_flat, line->backSector()->getCeilingTex())) quad.flags |= SKY;
@@ -1547,7 +1557,9 @@ void MapRenderer3D::updateLine(unsigned index)
 		}
 
 		// Texture scale
-		sx = sy = 1;
+		quad.texture = MapEditor::textureManager().getTexture(line->s2()->getTexLower(), mixed);
+		sx = quad.texture->getScaleX();
+		sy = quad.texture->getScaleY();
 		if (map->currentFormat() == MAP_UDMF && Game::configuration().featureSupported(UDMFFeature::TextureScaling))
 		{
 			if (line->s2()->hasProp("scalex_bottom"))
@@ -1555,8 +1567,10 @@ void MapRenderer3D::updateLine(unsigned index)
 			if (line->s2()->hasProp("scaley_bottom"))
 				sy = 1.0 / line->s2()->floatProperty("scaley_bottom");
 		}
-		xoff *= sx;
-		yoff *= sy;
+		if (!quad.texture->worldPanning()) {
+			xoff *= sx;
+			yoff *= sy;
+		}
 
 		if (lpeg)	// Lower unpegged
 			yoff += (ceiling2 - floor1);
@@ -1566,7 +1580,6 @@ void MapRenderer3D::updateLine(unsigned index)
 		quad.colour = colour2;
 		quad.fogcolour = fogcolour2;
 		quad.light = light2;
-		quad.texture = MapEditor::textureManager().getTexture(line->s2()->getTexLower(), mixed);
 		setupQuadTexCoords(&quad, length, xoff, yoff, floor1, floor2, false, sx, sy);
 		if (S_CMPNOCASE(sky_flat, line->frontSector()->getFloorTex())) quad.flags |= SKY;
 		quad.flags |= BACK;
@@ -1598,7 +1611,8 @@ void MapRenderer3D::updateLine(unsigned index)
 		}
 
 		// Texture scale
-		sx = sy = 1;
+		sx = quad.texture->getScaleX();
+		sy = quad.texture->getScaleY();
 		if (map->currentFormat() == MAP_UDMF && Game::configuration().featureSupported(UDMFFeature::TextureScaling))
 		{
 			if (line->s2()->hasProp("scalex_mid"))
@@ -1606,8 +1620,10 @@ void MapRenderer3D::updateLine(unsigned index)
 			if (line->s2()->hasProp("scaley_mid"))
 				sy = 1.0 / line->s2()->floatProperty("scaley_mid");
 		}
-		xoff *= sx;
-		yoff *= sy;
+		if (!quad.texture->worldPanning()) {
+			xoff *= sx;
+			yoff *= sy;
+		}
 
 		// Setup quad coordinates
 		double top, bottom;
@@ -1674,7 +1690,9 @@ void MapRenderer3D::updateLine(unsigned index)
 		}
 
 		// Texture scale
-		sx = sy = 1;
+		quad.texture = MapEditor::textureManager().getTexture(line->s2()->getTexUpper(), mixed);
+		sx = quad.texture->getScaleX();
+		sy = quad.texture->getScaleY();
 		if (map->currentFormat() == MAP_UDMF && Game::configuration().featureSupported(UDMFFeature::TextureScaling))
 		{
 			if (line->s2()->hasProp("scalex_top"))
@@ -1682,15 +1700,16 @@ void MapRenderer3D::updateLine(unsigned index)
 			if (line->s2()->hasProp("scaley_top"))
 				sy = 1.0 / line->s2()->floatProperty("scaley_top");
 		}
-		xoff *= sx;
-		yoff *= sy;
+		if (!quad.texture->worldPanning()) {
+			xoff *= sx;
+			yoff *= sy;
+		}
 
 		// Create quad
 		setupQuad(&quad, line->x2(), line->y2(), line->x1(), line->y1(), cp2, cp1);
 		quad.colour = colour2;
 		quad.fogcolour = fogcolour2;
 		quad.light = light2;
-		quad.texture = MapEditor::textureManager().getTexture(line->s2()->getTexUpper(), mixed);
 		setupQuadTexCoords(&quad, length, xoff, yoff, ceiling2, ceiling1, !upeg, sx, sy);
 		if (S_CMPNOCASE(sky_flat, line->frontSector()->getCeilingTex())) quad.flags |= SKY;
 		quad.flags |= BACK;
