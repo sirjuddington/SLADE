da5b7fd5b4ee3e5773ad2b199aceca5ec6cd6c03
Simon Judd <sirjuddington@gmail.com>
2018-12-18 12:48:39 +1030

    Request 24bit depth buffer for SFML OpenGL context
    This stops the SFML error spam in the log file (is a 32bit depth buffer even a thing anyway?)

--- src/UI/Canvas/OGLCanvas.cpp
+++ src/UI/Canvas/OGLCanvas.cpp
@@ -149,7 +149,7 @@ void OGLCanvas::createSFML()
 #endif
 	// Context settings
 	sf::ContextSettings settings;
-	settings.depthBits = 32;
+	settings.depthBits = 24;
 	settings.stencilBits = 8;
 	sf::RenderWindow::create(handle, settings);
 #endif
