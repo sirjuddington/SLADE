1df792bf4977a6fcc8ec6e4592cf4f4f9c6a2dad
Simon Judd <sirjuddington@gmail.com>
2020-02-17 14:37:17 +1030

    Update source file header comments to 2020

--- src/MapEditor/UI/GenLineSpecialPanel.cpp
+++ src/MapEditor/UI/GenLineSpecialPanel.cpp
@@ -1,7 +1,7 @@
 
 // -----------------------------------------------------------------------------
 // SLADE - It's a Doom Editor
-// Copyright(C) 2008 - 2019 Simon Judd
+// Copyright(C) 2008 - 2020 Simon Judd
 //
 // Email:       sirjuddington@gmail.com
 // Web:         https://slade.mancubus.net
