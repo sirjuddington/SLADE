769521d43491b2537ef2bf8081f834d35e18c47c
Kevin Caccamo <kevin@ciinet.org>
2020-03-18 19:09:46 -0400

    Rewrite MathStuff::angle2DRad to be shorter
    This code is much shorter, possibly easier to understand, and possibly more efficient than the previous implementation.

--- src/Utility/MathStuff.cpp
+++ src/Utility/MathStuff.cpp
@@ -262,51 +262,20 @@ double MathStuff::distanceRayLine(fpoint2_t r1, fpoint2_t r2, fpoint2_t s1, fpoi
  *******************************************************************/
 double MathStuff::angle2DRad(fpoint2_t p1, fpoint2_t p2, fpoint2_t p3)
 {
-	// From: http://stackoverflow.com/questions/3486172/angle-between-3-points
-	// modified not to bother converting to degrees
-	fpoint2_t ab(p2.x - p1.x, p2.y - p1.y);
-	fpoint2_t cb(p2.x - p3.x, p2.y - p3.y);
-
-	// dot product
-	double dot = (ab.x * cb.x + ab.y * cb.y);
-
-	// length square of both vectors
-	double abSqr = ab.x * ab.x + ab.y * ab.y;
-	double cbSqr = cb.x * cb.x + cb.y * cb.y;
-
-	// square of cosine of the needed angle
-	double cosSqr = dot * dot / abSqr / cbSqr;
-
-	// this is a known trigonometric equality:
-	// cos(alpha * 2) = [ cos(alpha) ]^2 * 2 - 1
-	double cos2 = 2 * cosSqr - 1;
-
-	// Here's the only invocation of the heavy function.
-	// It's a good idea to check explicitly if cos2 is within [-1 .. 1] range
-	double alpha2 =
-		(cos2 <= -1) ? PI :
-		(cos2 >= 1) ? 0 :
-		acosf(cos2);
-
-	//double rslt = alpha2 / 2;
-	//double rs = rslt * rad2deg;
-	double rs = alpha2 / 2;
-
-	// Now revolve the ambiguities.
-	// 1. If dot product of two vectors is negative - the angle is definitely
-	// above 90 degrees. Still we have no information regarding the sign of the angle.
-
-	// NOTE: This ambiguity is the consequence of our method: calculating the cosine
-	// of the double angle. This allows us to get rid of calling sqrt.
-	if (dot < 0)
-		rs = PI - rs;
-
-	// 2. Determine the sign. For this we'll use the Determinant of two vectors.
-	double det = (ab.x * cb.y - ab.y * cb.x);
-	if (det < 0)
-		rs = (2*PI) - rs;
-
-	return rs;
+	// Rewritten to be simpler by Talon1024, based on the implementation in:
+	// https://github.com/pineapplemachine/jsdoom/blob/8bf18cc7921af0dd1994eb55155b22779b2a874e/src/convert/3DMapBuilder.ts#L164
+	// Assume p2 is the "center" point, and that the angle to calculate is
+	// counterclockwise.
+	fpoint2_t ab(p1.x - p2.x, p1.y - p2.y);
+	fpoint2_t cb(p3.x - p2.x, p3.y - p2.y);
+	// Normalize vectors
+	ab.normalize();
+	cb.normalize();
+	// Dot and cross product of the two vectors
+	double dot = ab.dot(cb);
+	double cross = ab.cross(cb);
+	// Angle - will always be positive
+	return atan2(-cross, -dot) + PI;
 }
 
 /* MathStuff::rotatePoint
