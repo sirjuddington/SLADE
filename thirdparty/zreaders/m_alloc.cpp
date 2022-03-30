f95fe80849b1b499bf038ab33abd8b48d3fe8643
Simon Judd <sirjuddington@gmail.com>
2022-03-30 22:45:26 +1030

    Fix zreaders/m_alloc.cpp not having a default memory allocation path
    Applied the patch (manually) in #1274, seems to still work fine in windows, hopefully doesn't break anywhere else
    
    Resolves #1274

--- thirdparty/zreaders/m_alloc.cpp
+++ thirdparty/zreaders/m_alloc.cpp
@@ -57,14 +57,16 @@ namespace GC
 #endif
 #if defined(__APPLE__)
 #define _msize(p)				malloc_size(p)
-#elif defined(__sun)
-#define _msize(p)				(*((size_t*)(p)-1))
-#elif !defined(_WIN32)
+#elif defined(__FreeBSD__) || defined(__linux__)
 #define _msize(p)				malloc_usable_size(p)	// from glibc/FreeBSD
+#elif !defined(_WIN32)
+// For all other OS not handled above
+#define M_ALLOC_GENERIC  1
+#define _msize(p)                              (*((size_t*)(p)-1))
 #endif
 
 #ifndef _DEBUG
-#if !defined(__sun)
+#if !M_ALLOC_GENERIC
 void *M_Malloc(size_t size)
 {
 	void *block = malloc(size);
@@ -134,7 +136,7 @@ void *M_Realloc(void *memblock, size_t size)
 #include <crtdbg.h>
 #endif
 
-#if !defined(__sun)
+#if !M_ALLOC_GENERIC
 void *M_Malloc_Dbg(size_t size, const char *file, int lineno)
 {
 	void *block = _malloc_dbg(size, _NORMAL_BLOCK, file, lineno);
@@ -202,7 +204,7 @@ void *M_Realloc_Dbg(void *memblock, size_t size, const char *file, int lineno)
 #endif
 #endif
 
-#if !defined(__sun)
+#if !M_ALLOC_GENERIC
 void M_Free (void *block)
 {
 	if (block != NULL)
