497a3eecb94f7f22695350be0c881072fabcd5c4
Simon Judd <sirjuddington@gmail.com>
2022-04-07 11:58:25 +0930

    Fix wrong fmt include paths

--- src/General/CVar.cpp
+++ src/General/CVar.cpp
@@ -32,7 +32,7 @@
 // -----------------------------------------------------------------------------
 #include "Main.h"
 #include "Utility/StringUtils.h"
-#include <fmt/format.h>
+#include "thirdparty/fmt/include/fmt/format.h"
 
 using namespace slade;
 
--- src/TextEditor/TextStyle.cpp
+++ src/TextEditor/TextStyle.cpp
@@ -43,7 +43,7 @@
 #include "Utility/Parser.h"
 #include "Utility/StringUtils.h"
 #include "Utility/Tokenizer.h"
-#include <fmt/format.h>
+#include "thirdparty/fmt/include/fmt/format.h"
 
 using namespace slade;
 
