dff6de494754dd908464e7d0bf06d4ab839cadc5
Simon Judd <sirjuddington@gmail.com>
2023-05-26 23:24:23 +0930

    Fix table creation sql files
    Must have forgotten to add the one for archive_map, also archive_file needs an id column specified

--- dist/res/database/tables/archive_file.sql
+++ dist/res/database/tables/archive_file.sql
@@ -1,8 +1,8 @@
 BEGIN;
 
 CREATE TABLE archive_file (
-    path          TEXT     NOT NULL     -- Path to the archive file on disk (or directory), must be unique in the table
-                           PRIMARY KEY,
+	id            INTEGER  PRIMARY KEY,
+	path          TEXT     UNIQUE,		-- Path to the archive file on disk (or directory), must be unique in the table
     size          INTEGER,              -- Size of the file on disk (in bytes)
     hash          TEXT,                 -- Hash (xxHash 128-bit) of the file data on disk
     format_id     TEXT,                 -- Archive format id (eg. 'wad')
