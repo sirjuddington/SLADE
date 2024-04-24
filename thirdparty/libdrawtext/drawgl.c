ec28b686e33f0ba80332a2def5140b68201b2512
Simon Judd <sirjuddington@gmail.com>
2024-04-24 18:53:18 +0930

    Update libdrawtext to latest
    Looks like this is the only change, should hopefully fix macOS builds

--- thirdparty/libdrawtext/drawgl.c
+++ thirdparty/libdrawtext/drawgl.c
@@ -1,6 +1,6 @@
 /*
 libdrawtext - a simple library for fast text rendering in OpenGL
-Copyright (C) 2011-2018  John Tsiombikas <nuclear@member.fsf.org>
+Copyright (C) 2011-2023  John Tsiombikas <nuclear@member.fsf.org>
 
 This program is free software: you can redistribute it and/or modify
 it under the terms of the GNU Lesser General Public License as published by
@@ -66,6 +66,7 @@ static void add_glyph(struct glyph *g, float x, float y);
 
 #ifdef __APPLE__
 #include <OpenGL/gl.h>
+#include <OpenGL/glu.h>
 
 #else
 
