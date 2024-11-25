6f67337b6037f5db514023ea4a5d191869051e14
Simon Judd <sirjuddington@gmail.com>
2024-11-25 21:34:13 +1030

    Keep map object index order intact when removing
    This is a lot less efficient, but I think overall you'd usually want to keep relative object indices (eg. deleting vertex #2 should move all later vertices down 1 index, not swap in the last vertex to #2)

--- src/SLADEMap/MapObjectList/MapObjectList.h
+++ src/SLADEMap/MapObjectList/MapObjectList.h
@@ -33,42 +33,67 @@ public:
 	T*                first() const { return objects_.empty() ? nullptr : objects_[0]; }
 	T*                last() const { return objects_.empty() ? nullptr : objects_.back(); }
 
-	// Modification
+
+	// Modification ------------------------------------------------------------
+
+	// Add [object] to the list
 	virtual void add(T* object)
 	{
 		objects_.push_back(object);
 		++count_;
 	}
+
+	// Remove object at [index]
 	virtual void remove(unsigned index)
 	{
 		if (index < count_)
 		{
-			objects_[index] = objects_.back();
-			objects_.pop_back();
+			objects_.erase(objects_.begin() + index);
 			--count_;
 		}
 	}
+
+	// Remove [object] from the list
+	virtual void remove(T* object)
+	{
+		auto it = std::find(objects_.begin(), objects_.end(), object);
+		if (it != objects_.end())
+		{
+			objects_.erase(it);
+			--count_;
+		}
+	}
+
+	// Remove objects at [indices]
+	virtual void remove(const vector<unsigned>& indices)
+	{
+		// Null out objects to remove
+		auto removed_count = 0;
+		for (auto index : indices)
+			if (index < count_ && objects_[index])
+			{
+				objects_[index] = nullptr;
+				removed_count++;
+			}
+
+		// Rebuild list
+		auto old = objects_;
+		objects_.clear();
+		objects_.resize(old.size() - removed_count);
+		auto index = 0;
+		for (auto& object : old)
+			if (object)
+				objects_[index++] = object;
+
+		count_ = objects_.size();
+	}
+
+	// Remove last object
 	virtual void removeLast()
 	{
 		objects_.pop_back();
 		--count_;
 	}
-
-	// Misc
-	/*void putModifiedObjects(long since, vector<MapObject*>& modified_objects) const
-	{
-		for (const auto& object : objects_)
-			if (object->modifiedTime() >= since)
-				modified_objects.push_back(object);
-	}
-	bool modifiedSince(long since) const
-	{
-		for (const auto& object : objects_)
-			if (object->modifiedTime() > since)
-				return true;
-
-		return false;
-	}*/
 
 protected:
 	vector<T*> objects_;
