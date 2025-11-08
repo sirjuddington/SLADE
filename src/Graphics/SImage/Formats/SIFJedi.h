69b3c7c7da029ef89e08b38bb1950fa7deecc046
Simon Judd <sirjuddington@gmail.com>
2025-11-09 00:44:07 +1030

    Some cleanup

--- src/Graphics/SImage/Formats/SIFJedi.h
+++ src/Graphics/SImage/Formats/SIFJedi.h
@@ -59,6 +59,77 @@ inline bool JediRLE0(const uint8_t* src, uint32_t coloffs, int width, int height
 
 class SIFJediBM : public SIFormat
 {
+public:
+	SIFJediBM() : SIFormat("jedi_bm")
+	{
+		name_        = "Jedi BM";
+		extension_   = "dat";
+		reliability_ = 80;
+	}
+	~SIFJediBM() override = default;
+
+	bool isThisFormat(MemChunk& mc) override { return EntryDataFormat::format("img_jedi_bm")->isThisFormat(mc); }
+
+	SImage::Info info(MemChunk& mc, int index) override
+	{
+		SImage::Info info;
+		JediBMHeader header;
+		bool         transparent;
+		getImageInfo(info, mc, index, &header, transparent);
+		return info;
+	}
+
+protected:
+	bool readImage(SImage& image, MemChunk& data, int index) override
+	{
+		// Get image info
+		SImage::Info info;
+		JediBMHeader header;
+		bool         transparent;
+		unsigned     data_offset = getImageInfo(info, data, index, &header, transparent);
+
+		// Check valid
+		if (data_offset == 0)
+			return false;
+
+		// Create image (width/height swapped because column-major)
+		image.create(info.height, info.width, SImage::Type::PalMask);
+		image.fillAlpha(255);
+		uint8_t* img_data = imageData(image);
+		uint8_t* img_mask = imageMask(image);
+
+		if (header.rle == 0)
+		{
+			memcpy(img_data, data.data() + data_offset, info.width * info.height);
+		}
+		else if (header.rle == 1)
+		{
+			JediRLE(data.data() + data_offset, header.coloffs, info.width, info.height, img_data);
+		}
+		else if (header.rle == 2)
+		{
+			JediRLE0(data.data() + data_offset, header.coloffs, info.width, info.height, img_data);
+		}
+		else
+		{
+			global::error = "Jedi BM file: Invalid compression scheme";
+			return false;
+		}
+
+		// Manage transparency
+		if (transparent)
+		{
+			for (int i = 0; i < info.width * info.height; ++i)
+				if (img_data[i] == 0)
+					img_mask[i] = 0;
+		}
+
+		// Convert from column-major to row-major
+		image.rotate(270);
+
+		return true;
+	}
+
 private:
 	// Jedi bitmap header structs
 	struct JediBMHeader
@@ -124,18 +195,18 @@ private:
 		{
 			// 32 for the header, 2 for control data, then four bytes per subimage
 			int offset_offset = 34 + (info.imgindex << 2);
-			if (offset_offset + 4 > (int)mc.size())
+			if (offset_offset + 4 > static_cast<int>(mc.size()))
 			{
 				global::error = "Jedi BM file: invalid multi-BM file";
 				return 0;
 			}
-			const uint32_t*        header_poffset = (uint32_t*)(mc.data() + offset_offset);
-			uint32_t               header_offset  = wxINT32_SWAP_ON_BE(*header_poffset) + 34;
-			const JediBMSubheader* subheader      = (JediBMSubheader*)(mc.data() + header_offset);
-			transparent                           = !!(subheader->flag & 8);
-			info.width                            = wxINT16_SWAP_ON_BE(subheader->width);
-			info.height                           = wxINT16_SWAP_ON_BE(subheader->height);
-			data_offset                           = header_offset + 28;
+			auto header_poffset = reinterpret_cast<uint32_t*>(mc.data() + offset_offset);
+			auto header_offset  = wxINT32_SWAP_ON_BE(*header_poffset) + 34;
+			auto subheader      = reinterpret_cast<JediBMSubheader*>(mc.data() + header_offset);
+			transparent         = !!(subheader->flag & 8);
+			info.width          = wxINT16_SWAP_ON_BE(subheader->width);
+			info.height         = wxINT16_SWAP_ON_BE(subheader->height);
+			data_offset         = header_offset + 28;
 		}
 		else
 		{
@@ -146,81 +217,39 @@ private:
 		// Return data offset to image at [index]
 		return data_offset;
 	}
-
-protected:
-	bool readImage(SImage& image, MemChunk& data, int index) override
-	{
-		// Get image info
-		SImage::Info info;
-		JediBMHeader header;
-		bool         transparent;
-		unsigned     data_offset = getImageInfo(info, data, index, &header, transparent);
-
-		// Check valid
-		if (data_offset == 0)
-			return false;
-
-		// Create image (width/height swapped because column-major)
-		image.create(info.height, info.width, SImage::Type::PalMask);
-		image.fillAlpha(255);
-		uint8_t* img_data = imageData(image);
-		uint8_t* img_mask = imageMask(image);
-
-		if (header.rle == 0)
-		{
-			memcpy(img_data, data.data() + data_offset, info.width * info.height);
-		}
-		else if (header.rle == 1)
-		{
-			JediRLE(data.data() + data_offset, header.coloffs, info.width, info.height, img_data);
-		}
-		else if (header.rle == 2)
-		{
-			JediRLE0(data.data() + data_offset, header.coloffs, info.width, info.height, img_data);
-		}
-		else
-		{
-			global::error = "Jedi BM file: Invalid compression scheme";
-			return false;
-		}
-
-		// Manage transparency
-		if (transparent)
-		{
-			for (int i = 0; i < info.width * info.height; ++i)
-				if (img_data[i] == 0)
-					img_mask[i] = 0;
-		}
-
-		// Convert from column-major to row-major
-		image.rotate(270);
-
-		return true;
-	}
-
-public:
-	SIFJediBM() : SIFormat("jedi_bm")
-	{
-		name_        = "Jedi BM";
-		extension_   = "dat";
-		reliability_ = 80;
-	}
-	~SIFJediBM() override = default;
-
-	bool isThisFormat(MemChunk& mc) override { return EntryDataFormat::format("img_jedi_bm")->isThisFormat(mc); }
-
-	SImage::Info info(MemChunk& mc, int index) override
-	{
-		SImage::Info info;
-		JediBMHeader header;
-		bool         transparent;
-		getImageInfo(info, mc, index, &header, transparent);
-		return info;
-	}
 };
 
 class SIFJediFME : public SIFormat
 {
+public:
+	SIFJediFME(string id = "jedi_fme") : SIFormat(id)
+	{
+		name_        = "Jedi FME";
+		extension_   = "dat";
+		reliability_ = 80;
+	}
+	~SIFJediFME() override = default;
+
+	bool isThisFormat(MemChunk& mc) override { return EntryDataFormat::format("img_jedi_fme")->isThisFormat(mc); }
+
+	SImage::Info info(MemChunk& mc, int index) override
+	{
+		// Read headers
+		SImage::Info info;
+		const auto   header1 = reinterpret_cast<JediFMEHeader1*>(mc.data());
+		const auto   header2 = reinterpret_cast<JediFMEHeader2*>(mc.data() + wxINT32_SWAP_ON_BE(header1->head2));
+
+		// Setup variables
+		info.offset_x  = 0 - wxINT32_SWAP_ON_BE(header1->offsx);
+		info.offset_y  = 0 - wxINT32_SWAP_ON_BE(header1->offsy);
+		info.width     = wxINT32_SWAP_ON_BE(header2->width);
+		info.height    = wxINT32_SWAP_ON_BE(header2->height);
+		info.colformat = SImage::Type::PalMask;
+		info.format    = id_;
+
+		return info;
+	}
+
 protected:
 	struct JediFMEHeader1
 	{
@@ -244,10 +273,10 @@ protected:
 
 	bool readFrame(SImage& image, MemChunk& data, unsigned offset, unsigned numimages = 1) const
 	{
-		SImage::Info          info;
-		const JediFMEHeader1* header1 = (JediFMEHeader1*)(data.data() + offset);
-		const JediFMEHeader2* header2 = (JediFMEHeader2*)(data.data() + wxINT32_SWAP_ON_BE(header1->head2));
-		bool                  flip    = !!(header1->flag & 1);
+		SImage::Info info;
+		const auto   header1 = reinterpret_cast<JediFMEHeader1*>(data.data() + offset);
+		const auto   header2 = reinterpret_cast<JediFMEHeader2*>(data.data() + wxINT32_SWAP_ON_BE(header1->head2));
+		bool         flip    = !!(header1->flag & 1);
 
 		info.offset_x  = 0 - wxINT32_SWAP_ON_BE(header1->offsx);
 		info.offset_y  = 0 - wxINT32_SWAP_ON_BE(header1->offsy);
@@ -287,137 +316,10 @@ protected:
 	}
 
 	bool readImage(SImage& image, MemChunk& data, int index) override { return readFrame(image, data, 0); }
-
-public:
-	SIFJediFME(string id = "jedi_fme") : SIFormat(id)
-	{
-		name_        = "Jedi FME";
-		extension_   = "dat";
-		reliability_ = 80;
-	}
-	~SIFJediFME() override = default;
-
-	bool isThisFormat(MemChunk& mc) override { return EntryDataFormat::format("img_jedi_fme")->isThisFormat(mc); }
-
-	SImage::Info info(MemChunk& mc, int index) override
-	{
-		// Read headers
-		SImage::Info          info;
-		const JediFMEHeader1* header1 = (JediFMEHeader1*)(mc.data());
-		const JediFMEHeader2* header2 = (JediFMEHeader2*)(mc.data() + wxINT32_SWAP_ON_BE(header1->head2));
-
-		// Setup variables
-		info.offset_x  = 0 - wxINT32_SWAP_ON_BE(header1->offsx);
-		info.offset_y  = 0 - wxINT32_SWAP_ON_BE(header1->offsy);
-		info.width     = wxINT32_SWAP_ON_BE(header2->width);
-		info.height    = wxINT32_SWAP_ON_BE(header2->height);
-		info.colformat = SImage::Type::PalMask;
-		info.format    = id_;
-		return info;
-	}
 };
 
 class SIFJediWAX : public SIFJediFME
 {
-private:
-	// Jedi engine wax header structs
-	struct JediWAXHeader
-	{
-		uint32_t version;   // Constant worth 0x00100100
-		uint32_t numseqs;   // Number of sequences
-		uint32_t numframes; // Number of frames
-		uint32_t numcells;  // Number of cells
-		uint32_t pad[4];    // Unused values
-		uint32_t waxes[32]; // Offsets to the WAX subheaders
-	};
-	struct JediWAXSubheader
-	{
-		uint32_t width;    // Scaled width, in world units
-		uint32_t height;   // Scaled height, in world units
-		uint32_t fps;      // Frames per second of animation
-		uint32_t pad[4];   // Unused values, must be zero
-		uint32_t seqs[32]; // Offsets to sequences
-	};
-	struct JediWAXSequence
-	{
-		uint32_t pad[4];     // Unused values, must be zero
-		uint32_t frames[32]; // Offsets to frames
-	};
-
-	void readFrameOffsets(MemChunk& data, vector<unsigned>& offsets)
-	{
-		const JediWAXHeader* header = (JediWAXHeader*)data.data();
-		vector<size_t>       frameoffs;
-
-		// This is a recursive nightmare. What were the LucasArts devs smoking when they specced this format?
-		for (int i = 0; i < 32; ++i)
-		{
-			uint32_t waxi = wxINT32_SWAP_ON_BE(header->waxes[i]);
-			if (waxi > sizeof(header) && waxi < data.size() - sizeof(JediWAXSubheader))
-			{
-				const JediWAXSubheader* subhdr = (JediWAXSubheader*)(data.data() + waxi);
-				for (int j = 0; j < 32; ++j)
-				{
-					uint32_t seqj = wxINT32_SWAP_ON_BE(subhdr->seqs[j]);
-					if (seqj > sizeof(header) && seqj < data.size() - sizeof(JediWAXSequence))
-					{
-						const JediWAXSequence* seq = (JediWAXSequence*)(data.data() + seqj);
-						for (int k = 0; k < 32; ++k)
-						{
-							uint32_t framk = wxINT32_SWAP_ON_BE(seq->frames[k]);
-							if (framk > sizeof(header) && framk < data.size() - sizeof(JediWAXSequence))
-							{
-								const JediFMEHeader1* frame = (JediFMEHeader1*)(data.data() + framk);
-								uint32_t              cell  = wxINT32_SWAP_ON_BE(frame->head2);
-								if (cell > sizeof(header) && cell < data.size() - sizeof(JediFMEHeader2))
-								{
-									bool notfound = true;
-									for (size_t l = 0; l < frameoffs.size(); ++l)
-									{
-										if (frameoffs[l] == framk)
-										{
-											notfound = false;
-											break;
-										}
-									}
-									if (notfound)
-										frameoffs.push_back(framk);
-								}
-							}
-						}
-					}
-				}
-			}
-		}
-		// Urgh. At least it's over now.
-
-		offsets.reserve(frameoffs.size());
-		for (auto off : frameoffs)
-			offsets.push_back(static_cast<unsigned>(off));
-	}
-
-protected:
-	bool readImage(SImage& image, MemChunk& data, int index) override
-	{
-		// Determine all frame offsets
-		vector<unsigned> offsets;
-		readFrameOffsets(data, offsets);
-
-		// Sanitize index if needed
-		int numimages = offsets.size();
-		if (offsets.empty())
-		{
-			global::error = "Jedi WAX error: No cell found in wax!"; // What a surreal error message ;)
-			return false;
-		}
-		index %= numimages;
-		if (index < 0)
-			index = numimages + index;
-
-		// Load image
-		return readFrame(image, data, offsets[index], numimages);
-	}
-
 public:
 	SIFJediWAX() : SIFJediFME("jedi_wax")
 	{
@@ -447,8 +349,8 @@ public:
 			info.imgindex = info.numimages + info.imgindex;
 
 		// Read headers
-		const JediFMEHeader1* header1 = (JediFMEHeader1*)(mc.data() + offsets[info.imgindex]);
-		const JediFMEHeader2* header2 = (JediFMEHeader2*)(mc.data() + wxINT32_SWAP_ON_BE(header1->head2));
+		const auto header1 = reinterpret_cast<JediFMEHeader1*>(mc.data() + offsets[info.imgindex]);
+		const auto header2 = reinterpret_cast<JediFMEHeader2*>(mc.data() + wxINT32_SWAP_ON_BE(header1->head2));
 
 		// Setup variables
 		info.offset_x  = 0 - wxINT32_SWAP_ON_BE(header1->offsx);
@@ -459,5 +361,101 @@ public:
 		info.format    = id_;
 
 		return info;
+	}
+
+protected:
+	bool readImage(SImage& image, MemChunk& data, int index) override
+	{
+		// Determine all frame offsets
+		vector<unsigned> offsets;
+		readFrameOffsets(data, offsets);
+
+		// Sanitize index if needed
+		int numimages = offsets.size();
+		if (offsets.empty())
+		{
+			global::error = "Jedi WAX error: No cell found in wax!"; // What a surreal error message ;)
+			return false;
+		}
+		index %= numimages;
+		if (index < 0)
+			index = numimages + index;
+
+		// Load image
+		return readFrame(image, data, offsets[index], numimages);
+	}
+
+private:
+	// Jedi engine wax header structs
+	struct JediWAXHeader
+	{
+		uint32_t version;   // Constant worth 0x00100100
+		uint32_t numseqs;   // Number of sequences
+		uint32_t numframes; // Number of frames
+		uint32_t numcells;  // Number of cells
+		uint32_t pad[4];    // Unused values
+		uint32_t waxes[32]; // Offsets to the WAX subheaders
+	};
+	struct JediWAXSubheader
+	{
+		uint32_t width;    // Scaled width, in world units
+		uint32_t height;   // Scaled height, in world units
+		uint32_t fps;      // Frames per second of animation
+		uint32_t pad[4];   // Unused values, must be zero
+		uint32_t seqs[32]; // Offsets to sequences
+	};
+	struct JediWAXSequence
+	{
+		uint32_t pad[4];     // Unused values, must be zero
+		uint32_t frames[32]; // Offsets to frames
+	};
+
+	void readFrameOffsets(MemChunk& data, vector<unsigned>& offsets)
+	{
+		const auto     header = reinterpret_cast<JediWAXHeader*>(data.data());
+		vector<size_t> frameoffs;
+
+		// This is a recursive nightmare. What were the LucasArts devs smoking when they specced this format?
+		for (auto wax_off : header->waxes)
+		{
+			if (wax_off > sizeof(JediWAXHeader) && wax_off < data.size() - sizeof(JediWAXSubheader))
+			{
+				const auto subhdr = reinterpret_cast<JediWAXSubheader*>(data.data() + wax_off);
+				for (auto seq_off : subhdr->seqs)
+				{
+					if (seq_off > sizeof(JediWAXHeader) && seq_off < data.size() - sizeof(JediWAXSequence))
+					{
+						const auto seq = reinterpret_cast<JediWAXSequence*>(data.data() + seq_off);
+						for (auto frame_off : seq->frames)
+						{
+							if (frame_off > sizeof(JediWAXHeader) && frame_off < data.size() - sizeof(JediWAXSequence))
+							{
+								const auto frame = reinterpret_cast<JediFMEHeader1*>(data.data() + frame_off);
+								uint32_t   cell  = wxINT32_SWAP_ON_BE(frame->head2);
+								if (cell > sizeof(JediWAXHeader) && cell < data.size() - sizeof(JediFMEHeader2))
+								{
+									bool notfound = true;
+									for (auto frameoff : frameoffs)
+									{
+										if (frameoff == frame_off)
+										{
+											notfound = false;
+											break;
+										}
+									}
+									if (notfound)
+										frameoffs.push_back(frame_off);
+								}
+							}
+						}
+					}
+				}
+			}
+		}
+		// Urgh. At least it's over now.
+
+		offsets.reserve(frameoffs.size());
+		for (auto off : frameoffs)
+			offsets.push_back(static_cast<unsigned>(off));
 	}
 };
